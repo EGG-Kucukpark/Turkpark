3fec1726a922788e781656b6c00ad150bb177a6e
Deniz Sultanoğlu <hallobro82@gmail.com>
2021-06-17 14:46:47 +0300

    master

---
+++ frontend/src/views/lab/asansor/client.vue
@@ -0,0 +1,470 @@
+<template>
+  <b-card title="Asansör">
+    <b-form-group
+      style="font-size: 18px"
+      label="Firma Seçiniz: "
+      label-cols-sm="1"
+    >
+      <b-form-select @change="select" v-model="Selected">
+        <option disabled value="">Lütfen Seçim Yapınız</option>
+        <option
+          v-bind:value="{ firma_email: firma.email }"
+          v-for="firma in firma"
+          :key="firma.id"
+        >
+          {{ firma.name }}
+        </option>
+      </b-form-select>
+    </b-form-group>
+
+    <b-row>
+      <b-col>
+        <b-form-group
+          label-cols-sm="7"
+          label-align-sm="left"
+          label-size="sm"
+          label-for="filterInput"
+          class="mb-1"
+        >
+          <b-input-group>
+            <b-form-input
+              id="filterInput"
+              v-model="filter"
+              type="search"
+              placeholder="........"
+            />
+          </b-input-group>
+        </b-form-group>
+      </b-col>
+
+      <span>
+        <b-button
+          class="mb-1"
+          style="margin-right: 50px"
+          variant="success"
+          @click="modal"
+          >Yeni Rapor</b-button
+        >
+
+        <b-modal
+          hide-header-close
+          :hide-footer="true"
+          size="lg"
+          ref="modal"
+          centered
+          title="Rapor Ekle"
+        >
+          <b-card>
+            <b-form @submit.prevent="submit">
+              <b-form-group
+                v-if="show"
+                style="font-size: 13px"
+                label="Firma: "
+                label-cols-sm="1"
+              >
+                <b-form-select @change="select2" v-model="firmaselected">
+                  <option disabled value="">Lütfen Seçim Yapınız</option>
+                  <option
+                    v-bind:value="{ firma_email: firma.email }"
+                    v-for="firma in firma"
+                    :key="firma.id"
+                  >
+                    {{ firma.name }}
+                  </option>
+                </b-form-select>
+              </b-form-group>
+
+              <b-form-group
+                style="font-size: 13px"
+                label="Çalışan: "
+                label-cols-sm="1"
+              >
+                <b-form-select v-model="calisanselected">
+                  <option disabled value="">Lütfen Seçim Yapınız</option>
+                  <option
+                    v-bind:value="{ name: calisan.name, id: calisan.id }"
+                    v-for="calisan in calisan"
+                    :key="calisan.id"
+                  >
+                    {{ calisan.name }}
+                  </option>
+                </b-form-select>
+              </b-form-group>
+
+              <b-form-group
+                style="font-size: 13px"
+                label="Rapor: "
+                label-cols-sm="1"
+              >
+                <b-form-file
+                  @change.prevent="change"
+                  v-model="file"
+                  placeholder=" Bir dosya seçin veya buraya sürükleyin..."
+                  drop-placeholder="Drop file here..."
+                  accept=".jpg, .png, .pdf, "
+                />
+              </b-form-group>
+
+              <div style="float: right">
+                <b-button variant="success" type="submit">
+                  Rapor Ekle
+                </b-button>
+              </div>
+              <div style="float: right; padding-right: 10px" @click="form()">
+                <b-button variant="danger" @click="form()"> İptal</b-button>
+              </div>
+            </b-form>
+
+            <!-- Emulate built in modal footer ok and cancel button actions -->
+          </b-card>
+        </b-modal>
+      </span>
+
+     <b-col cols="12" class="table-responsive">
+        <b-table
+          striped
+          hover
+          responsive
+          :per-page="perPage"
+          :current-page="currentPage"
+          :items="items"
+          :fields="fields"
+          :sort-by.sync="sortBy"
+          :sort-desc.sync="sortDesc"
+          :sort-direction="sortDirection"
+          :filter="filter"
+          :filter-included-fields="filterOn"
+          @filtered="onFiltered"
+           show-empty
+          empty-text="Veri Bulunamadı."
+          empty-filtered-text="Veri Bulunamadı."
+        >
+          <template #cell(role)="data">
+            <div class="text-nowrap">
+              <feather-icon
+                :icon="roleVariant(data.item.role)"
+                size="18"
+                class="mr-50"
+                style="color: red"
+              />
+              <span class="align-text-top text-capitalize">{{
+                data.item.role
+              }}</span>
+            </div>
+          </template>
+
+          <template #cell(email_verified_at)="data">
+            <b-badge
+              v-if="data.item.email_verified_at != null"
+              variant="light-success"
+            >
+              <span> Onaylanmış</span>
+            </b-badge>
+
+            <b-badge
+              v-if="data.item.email_verified_at === null"
+              variant="light-danger"
+            >
+              <span> Onaylanmamış</span>
+            </b-badge>
+          </template>
+
+          <template #cell(status)="data">
+            <b-badge :variant="statusVariant(data.item.status)">
+              <span v-if="data.item.status == 1"> Aktif</span>
+              <span v-if="data.item.status == 2"> Beklemede</span>
+              <span v-if="data.item.status == 0"> Red</span>
+            </b-badge>
+          </template>
+
+          <p style="text-align: center; width: 100%" show-empty>
+            >
+            <b> Kullanıcı Bulunamadı.</b>
+          </p>
+
+          <template #cell(actions)="data">
+            <span>
+              <b-button
+                variant="success"
+                @click.prevent="indir(data.item.dosya)"
+              >
+                İndir
+              </b-button>
+              <b-button v-if="show" variant="warning"> Sil </b-button>
+              <b-button
+                variant="danger"
+                @click.prevent="göster(data.item.dosya)"
+              >
+                Göster
+              </b-button>
+            </span>
+          </template>
+        </b-table>
+      </b-col>
+
+      <b-col md="2" sm="4" class="my-1">
+        <b-form-group class="mb-0">
+          <b-form-select
+            id="perPageSelect"
+            v-model="perPage"
+            size="sm"
+            :options="pageOptions"
+            class="w-50"
+          />
+        </b-form-group>
+      </b-col>
+
+      <b-col cols="12">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="totalRows"
+          :per-page="perPage"
+          align="center"
+          size="sm"
+          class="my-0"
+        />
+      </b-col>
+    </b-row>
+  </b-card>
+</template>
+
+<script>
+import ToastificationContent from "@core/components/toastification/ToastificationContent.vue";
+
+import {
+  BTable,
+  BAvatar,
+  BBadge,
+  BRow,
+  BCol,
+  BFormGroup,
+  BFormSelect,
+  BPagination,
+  BInputGroup,
+  BFormInput,
+  BInputGroupAppend,
+  BButton,
+  BCard,
+  BModal,
+  BForm,
+  BFormFile,
+} from "bootstrap-vue";
+import axios from "@axios";
+
+export default {
+  components: {
+    BTable,
+    BAvatar,
+    BBadge,
+    BRow,
+    BCol,
+    BFormGroup,
+    BFormSelect,
+    BPagination,
+    BInputGroup,
+    BFormInput,
+    BInputGroupAppend,
+    BButton,
+    BCard,
+    BModal,
+    ToastificationContent,
+    BForm,
+    BFormFile,
+  },
+  props: {
+    userData: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      perPage: 5,
+      pageOptions: [3, 5, 10],
+      totalRows: 1,
+      currentPage: 1,
+      sortBy: "",
+      sortDesc: false,
+      sortDirection: "asc",
+      filter: null,
+      filterOn: [],
+      infoModal: {
+        id: "info-modal",
+        title: "",
+        content: "",
+      },
+      fields: [
+        { key: "name", label: "Çalışan ismi", sortable: true, filter: true },
+        { key: "dosya", label: "Dosya Adı", sortable: true, filter: true },
+        { key: "created_at", label: "Tarih", sortable: true, filter: true },
+
+        { key: "actions", label: "Eylemler" },
+      ],
+      items: [],
+      id: null,
+      file: null,
+      show: true,
+      searchTerm: "",
+      Selected: "",
+      firma: [],
+      calisanselected: "",
+      firmaselected: "",
+      calisan: "",
+    };
+  },
+
+  computed: {
+    sortOptions() {
+      // Create an options list from our fields
+      return this.fields
+        .filter((f) => f.sortable)
+        .map((f) => ({ text: f.label, value: f.key }));
+    },
+  },
+  created() {
+    var user = JSON.parse(localStorage.getItem("user"));
+
+    if (user.role === "Client") {
+      this.show = false;
+      var mail = user.email;
+      axios
+        .post("/api/getfile", { firma_email: mail })
+        .then((res) => (this.rows = res.data));
+    } else {
+      axios.post("/api/firmalar").then((response) => {
+        this.firma = response.data;
+      });
+    }
+  },
+  mounted() {
+   setTimeout(() => {
+
+      this.totalRows = this.items.length;
+    }, 500);
+  },
+  methods: {
+    refreshStop() {
+      setTimeout(() => {
+        var email = this.firmaselected.firma_email;
+        this.Selected = {
+
+            firma_email: this.firmaselected.firma_email
+        }
+
+        axios
+          .post("/api/getfile", { firma_email: email, status: 1 })
+          .then((res) => (this.items = res.data))
+          .then(
+            this.$toast({
+              component: ToastificationContent,
+              position: "top-right",
+              props: {
+                title: `Rapor İşlemleri `,
+                icon: "FileTextIcon",
+                variant: "success",
+                text: ` İşlem Başarılı.`,
+              },
+            })
+          );
+      }, 2000);
+    },
+
+    change(event) {
+      this.file = event.target.files[0];
+    },
+
+    submit() {
+      const formData = new FormData();
+      formData.set("file", this.file);
+      formData.append("id", this.calisanselected.id);
+      formData.append("name", this.calisanselected.name);
+      formData.append("firma_email", this.firmaselected.firma_email);
+      formData.append('status', '1')
+      axios
+        .post("/api/belgeyukle", formData)
+        .then((res) => this.refreshStop())
+        .catch((error) => {
+          this.$toast({
+            component: ToastificationContent,
+            position: "top-right",
+            props: {
+              title: "Rapor İşlemleri",
+              icon: "FileTextIcon",
+              variant: "danger",
+              text: " İşlem Başarısız.",
+            },
+          });
+        })
+        .then(this.$refs["modal"].hide());
+    },
+
+
+
+   select() {
+
+
+      var email = this.Selected.firma_email;
+      axios
+        .post("/api/getfile", { firma_email: email, status: 1 })
+        .then((res) => (this.items = res.data));
+    },
+
+
+
+
+    göster(dosya) {
+      window.open("/Dosyalar/" + dosya, "_blank");
+    },
+
+    select2() {
+      var email = this.firmaselected.firma_email;
+      axios
+        .post("/api/calisanlar", { firma_email: email })
+        .then((res) => (this.calisan = res.data));
+    },
+    form() {
+      this.$refs["modal"].hide();
+      this.file == null;
+      this.firmaselected == null;
+      this.calisanselected == null;
+    },
+
+    indir(dosya) {
+      axios
+        .post(
+          "/api/indir",
+          { id: this.id, dosya: dosya },
+          { responseType: "blob" }
+        )
+        .then((response) => {
+          var data = response.data;
+          const url = window.URL.createObjectURL(new Blob([data]));
+          const link = document.createElement("a");
+          link.href = url;
+          link.setAttribute("download", dosya);
+          document.body.appendChild(link);
+          link.click();
+        });
+    },
+
+    modal() {
+      this.$refs["modal"].show();
+    },
+
+    info(item, index, button) {
+      this.infoModal.title = `Row index: ${index}`;
+      this.infoModal.content = JSON.stringify(item, null, 2);
+      this.$root.$emit("bv::show::modal", this.infoModal.id, button);
+    },
+    resetInfoModal() {
+      this.infoModal.title = "";
+      this.infoModal.content = "";
+    },
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      this.totalRows = filteredItems.length;
+      this.currentPage = 1;
+    },
+  },
+};
+</script>
--- frontend/src/views/lab/lab.vue
+++ frontend/src/views/lab/lab.vue
@@ -8,25 +8,25 @@
         @input="selected"
         style="margin-bottom: 30px"
       >
+        <option value="disabled" disabled>Lütfen Bir Seçim Yapınız</option>
         <option value="lab">Laboratuvar</option>
-        <option>Asansör</option>
+        <option value="asansor">Asansör</option>
         <option value="ölçüm">Ortam Ölçümleri</option>
-        <option>Periyodik Kontroller</option>
-        <option>İlkyardım</option>
-        <option>Eğitimler</option>
+        <option value="periyodik">Periyodik Kontroller</option>
+        <option value="ilkyardim">İlk Yardım</option>
+        <option value="egitimler">Eğitimler</option>
       </b-form-select>
     </b-card>
 
-    <b-alert variant="danger" v-if="sonuc === null">
+    <b-alert variant="danger" :show="sonuc === null">
       <h4 class="alert-heading">Veri Çekilemedi</h4>
       <div class="alert-body">
         Gösterilecek sonuç yok. Lütfen Yukarıdan sonuç tipini seçiniz.
       </div>
     </b-alert>
-
+    <!-- Tab: LAB -->
     <template v-if="sonuc === 'lab'">
       <b-tabs v-if="userData" pills>
-        <!-- Tab: Account -->
         <b-tab active>
           <template #title>
             <feather-icon
@@ -47,14 +47,12 @@
           </template>
           <individual :user-data="userData" class="mt-2 pt-75" />
         </b-tab>
-
-        <!-- Tab: Social -->
       </b-tabs>
     </template>
 
+    <!-- Tab: ÖLÇÜM -->
     <template v-if="sonuc === 'ölçüm'">
       <b-tabs v-if="userData" pills>
-        <!-- Tab: Account -->
         <b-tab active>
           <template #title>
             <feather-icon
@@ -67,13 +65,119 @@
           <olcumfirma :user-data="userData" class="mt-2 pt-75" />
         </b-tab>
 
-        <!-- Tab: Information -->
         <b-tab v-if="show">
           <template #title>
             <feather-icon icon="InfoIcon" size="16" class="mr-0 mr-sm-50" />
             <span class="d-none d-sm-inline">Bireysel Sonuçlar</span>
           </template>
           <olcumbirey :user-data="userData" class="mt-2 pt-75" />
+        </b-tab>
+      </b-tabs>
+    </template>
+
+    <!-- Tab: Egitimler -->
+    <template v-if="sonuc === 'egitimler'">
+      <b-tabs v-if="userData" pills>
+        <b-tab active>
+          <template #title>
+            <feather-icon
+              icon="BriefcaseIcon"
+              size="16"
+              class="mr-0 mr-sm-50"
+            />
+            <span class="d-none d-sm-inline"> Firma Sonuçları </span>
+          </template>
+          <egitimlerirma :user-data="userData" class="mt-2 pt-75" />
+        </b-tab>
+
+        <b-tab v-if="show">
+          <template #title>
+            <feather-icon icon="InfoIcon" size="16" class="mr-0 mr-sm-50" />
+            <span class="d-none d-sm-inline">Bireysel Sonuçlar</span>
+          </template>
+          <egitimlerirey :user-data="userData" class="mt-2 pt-75" />
+        </b-tab>
+      </b-tabs>
+    </template>
+
+    <template v-if="sonuc === 'asansor'">
+      <b-tabs v-if="userData" pills>
+        <!-- Tab: Account -->
+        <b-tab active>
+          <template #title>
+            <feather-icon
+              icon="BriefcaseIcon"
+              size="16"
+              class="mr-0 mr-sm-50"
+            />
+            <span class="d-none d-sm-inline"> Firma Sonuçları </span>
+          </template>
+          <asansorfirma :user-data="userData" class="mt-2 pt-75" />
+        </b-tab>
+
+        <!-- Tab: Information -->
+        <b-tab v-if="show">
+          <template #title>
+            <feather-icon icon="InfoIcon" size="16" class="mr-0 mr-sm-50" />
+            <span class="d-none d-sm-inline">Bireysel Sonuçlar</span>
+          </template>
+          <asansorbirey :user-data="userData" class="mt-2 pt-75" />
+        </b-tab>
+
+        <!-- Tab: Social -->
+      </b-tabs>
+    </template>
+
+    <template v-if="sonuc === 'periyodik'">
+      <b-tabs v-if="userData" pills>
+        <!-- Tab: Account -->
+        <b-tab active>
+          <template #title>
+            <feather-icon
+              icon="BriefcaseIcon"
+              size="16"
+              class="mr-0 mr-sm-50"
+            />
+            <span class="d-none d-sm-inline"> Firma Sonuçları </span>
+          </template>
+          <periyodikfirma :user-data="userData" class="mt-2 pt-75" />
+        </b-tab>
+
+        <!-- Tab: Information -->
+        <b-tab v-if="show">
+          <template #title>
+            <feather-icon icon="InfoIcon" size="16" class="mr-0 mr-sm-50" />
+            <span class="d-none d-sm-inline">Bireysel Sonuçlar</span>
+          </template>
+          <periyodikbirey :user-data="userData" class="mt-2 pt-75" />
+        </b-tab>
+
+        <!-- Tab: Social -->
+      </b-tabs>
+    </template>
+
+    <template v-if="sonuc === 'ilkyardim'">
+      <b-tabs v-if="userData" pills>
+        <!-- Tab: Account -->
+        <b-tab active>
+          <template #title>
+            <feather-icon
+              icon="BriefcaseIcon"
+              size="16"
+              class="mr-0 mr-sm-50"
+            />
+            <span class="d-none d-sm-inline"> Firma Sonuçları </span>
+          </template>
+          <ilkyardimfirma :user-data="userData" class="mt-2 pt-75" />
+        </b-tab>
+
+        <!-- Tab: Information -->
+        <b-tab v-if="show">
+          <template #title>
+            <feather-icon icon="InfoIcon" size="16" class="mr-0 mr-sm-50" />
+            <span class="d-none d-sm-inline">Bireysel Sonuçlar</span>
+          </template>
+          <ilkyardimbirey :user-data="userData" class="mt-2 pt-75" />
         </b-tab>
 
         <!-- Tab: Social -->
@@ -98,11 +202,24 @@ import {
   BFormSelect,
   BFormGroup,
 } from "bootstrap-vue";
+
 import individual from "./individual";
 import client from "./client";
 
+import asansorbirey from "./asansor/individual";
+import asansorfirma from "./asansor/client";
+
+import egitimlerirey from "./egitimler/individual";
+import egitimlerirma from "./egitimler/client";
+
+import periyodikbirey from "./periyodik/individual";
+import periyodikfirma from "./periyodik/client";
+
 import olcumbirey from "./measure/individual";
 import olcumfirma from "./measure/client";
+
+import ilkyardimbirey from "./ilkyardim/individual";
+import ilkyardimfirma from "./ilkyardim/client";
 
 export default {
   components: {
@@ -121,14 +238,22 @@ export default {
     // Local Components
 
     individual,
+    asansorbirey,
+    asansorfirma,
     client,
     olcumbirey,
     olcumfirma,
+    egitimlerirey,
+    egitimlerirma,
+    periyodikbirey,
+    periyodikfirma,
+    ilkyardimbirey,
+    ilkyardimfirma,
   },
   data() {
     return {
       show: true,
-      sonuclar: null,
+      sonuclar: "disabled",
       sonuc: null,
     };
   },
@@ -137,17 +262,6 @@ export default {
     if (user.role == "Client") {
       this.show = false;
     }
-
-    this.$toast({
-      component: ToastificationContent,
-      position: "top-right",
-      props: {
-        title: `Sonuç İşlemleri `,
-        icon: "FileTextIcon",
-        variant: "success",
-        text: ` Lütfen Sonuç Tipini Seçiniz`,
-      },
-    });
   },
   setup() {
     const userData = ref(null);
@@ -165,6 +279,18 @@ export default {
         console.log(this.sonuc);
       } else if (this.sonuclar === "ölçüm") {
         this.sonuc = "ölçüm";
+        console.log(this.sonuc);
+      } else if (this.sonuclar === "asansor") {
+        this.sonuc = "asansor";
+        console.log(this.sonuc);
+      } else if (this.sonuclar === "egitimler") {
+        this.sonuc = "egitimler";
+        console.log(this.sonuc);
+      } else if (this.sonuclar === "periyodik") {
+        this.sonuc = "periyodik";
+        console.log(this.sonuc);
+      } else if (this.sonuclar === "ilkyardim") {
+        this.sonuc = "ilkyardim";
         console.log(this.sonuc);
       }
     },
